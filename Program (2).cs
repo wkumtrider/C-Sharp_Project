d8a0077a619faffcc1d6a2ab955d8c388bd3bfac
jmccarty <breezer480@gmail.com>
2020-01-20 19:25:35 -0500

    Adding files to C# Project repository

---
+++ Program (2).cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace C__Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            sayHey("Hey Jay!");
+            sayHey("Hey Mel!");
+            sayHey("Hey Luna!");
+        }
+
+        static void sayHey(string words) 
+        {
+            Console.WriteLine(words);
+        }
+
+
+    }
+
+
+
+
+    
+}
