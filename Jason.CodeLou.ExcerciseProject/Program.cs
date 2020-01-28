8739a7f325c8f178c1ff6ce904a6c14f461d4448
jmccarty <breezer480@gmail.com>
2020-01-28 18:32:15 -0500

    Adding project 1, updated code

---
+++ Jason.CodeLou.ExcerciseProject/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Jason.CodeLou.ExcerciseProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string continueRunning = "Y";
+            while (continueRunning == "Y")
+            {
+               InputStudent();
+               Console.WriteLine("DO you want to add another student? (Y/N)");
+               continueRunning = Console.ReadLine();
+            }
+
+            Console.ReadKey();
+        }
+
+        static void InputStudent() 
+        {
+            Console.WriteLine("Enter Student Id");
+                var studentId = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter First Name");
+                var studentFirstName = Console.ReadLine();
+                Console.WriteLine("Enter Last Name");
+                var studentLastName = Console.ReadLine();
+                Console.WriteLine("Enter Class Name");
+                var className = Console.ReadLine();
+                Console.WriteLine("Enter Last Class Completed");
+                var lastClass = Console.ReadLine();
+                Console.WriteLine("Enter Last Class Completed Date in format MM/dd/YYYY");
+                var lastCompletedOn = DateTimeOffset.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Start Date in format MM/dd/YYYY");
+                var startDate = DateTimeOffset.Parse(Console.ReadLine());
+
+                var studentRecord = new Student();
+                studentRecord.StudentId = studentId;
+                studentRecord.FirstName = studentFirstName;
+                studentRecord.LastName = studentLastName;
+                studentRecord.ClassName = className;
+                studentRecord.StartDate = startDate;
+                studentRecord.LastClassCompleted = lastClass;
+                studentRecord.LastClassCompletedOn = lastCompletedOn;
+                Console.WriteLine($"Student Id | Name |  Class "); ;
+                Console.WriteLine($"{studentRecord.StudentId} | {studentRecord.FirstName} {studentRecord.LastName} | {studentRecord.ClassName} "); ;
+                
+               
+
+        }
+    }
+}
